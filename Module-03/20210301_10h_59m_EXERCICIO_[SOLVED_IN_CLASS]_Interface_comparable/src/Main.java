9d0be9f1f2794f007a3ffb51e5f693148604297f
Marcio Guilherme Bronzato de Avellar <mgb.avellar@gmail.com>
2021-03-01 11:49:18 -0300

    Module 03 - exercicio resolvido interface comparable - v2: com a interface comparable

--- Module-03/20210301_10h_59m_EXERCICIO_[SOLVED_IN_CLASS]_Interface_comparable/src/Main.java
+++ Module-03/20210301_10h_59m_EXERCICIO_[SOLVED_IN_CLASS]_Interface_comparable/src/Main.java
@@ -1,3 +1,5 @@
+import entities.Employee;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -23,23 +25,48 @@ Vou usar o versionamento do github.
 
          */
 
-        List<String> list = new ArrayList<>();
+        // List<String> list = new ArrayList<>();
 
-        String path = "/home/marcio/IdeaProjects/00-JAVA-NELIO-ALVES-JORNADA-BBCHAIN-PITANG/Module-03/nameList.txt";
+        List<Employee> list = new ArrayList<>();
+
+        String path = "/home/marcio/IdeaProjects/00-JAVA-NELIO-ALVES-JORNADA-BBCHAIN-PITANG/Module-03/nameList_v2.csv";
 
         System.out.println();
 
         try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 
-            String name = br.readLine();
+            //String name = br.readLine();
+            String employeeData = br.readLine();
 
-            while (name != null) {
-                list.add(name);
-                name = br.readLine();
+            while (employeeData != null) {
+                //list.add(name);
+
+                String[] fields = employeeData.split(",");
+
+                list.add(new Employee(fields[0], Double.parseDouble((fields[1]))));
+
+                employeeData = br.readLine();
             }
-            Collections.sort(list);
-            for (String s : list) {
-                System.out.println(s);
+
+            /*
+            O maior problema é que do jeito que estava, o Collections não estava aceitando
+            ordenar a lista de Employees, pois o método 'sort' não é aplicável para argumentos
+            do tipo Employee. Por isso tivemos que implementar a classe genérica Comparable na
+            classe Employee com o cabeçalho
+
+            public class Employee implements Comparable<Employee>
+
+            Esse comando diz que Employee é do tipo Comparable. Aí podemos usar os recursos
+            de Comparable.
+             */
+
+            Collections.sort(list); // Isso ordena a lista. Veja na classe Employee como
+                                    //    ordenar por nome ou por salário.
+
+
+            //for (String s : list) {
+            for (Employee emp : list) {
+                System.out.println(emp.getName() + ", " + emp.getSalary());
             }
         }
         catch (IOException e) {
