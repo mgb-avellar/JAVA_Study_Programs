11acca42089d9a755146c50b025e32f4f1b0a832
Marcio Guilherme Bronzato de Avellar <mgb.avellar@gmail.com>
2021-03-03 15:53:58 -0300

    Module 03 - cinco maneiras de predicate: v1: implementação da interface

---
+++ Module-03/20210303_15h_46m_EX_cinco_maneiras_predicate/src/Main.java
@@ -0,0 +1,34 @@
+import entities.Product;
+import utils.ProductPredicate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Notebook", 1200.00));
+        list.add(new Product("Tablet", 450.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("HD Case", 80.90));
+
+        list.removeIf(new ProductPredicate());
+
+        System.out.println();
+
+        for (Product p : list) {
+            System.out.println(p);
+        }
+
+
+    }
+
+
+}
