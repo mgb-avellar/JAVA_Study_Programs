1768141a5a04995aa102e433a8700173d252e75e
Marcio Guilherme Bronzato de Avellar <mgb.avellar@gmail.com>
2021-03-03 14:43:03 -0300

    Module 03 - seis maneiras de comparator: v1: diretamente na classe Product

---
+++ Module-03/20210303_14h_33m_EX_seis_maneiras_comparator/src/Main.java
@@ -0,0 +1,27 @@
+import entities.Product;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Notebook", 1200.00));
+        list.add(new Product("Tablet", 450.00));
+
+        Collections.sort(list);
+
+        System.out.println();
+
+        for (Product p : list) {
+            System.out.println(p);
+        }
+
+
+    }
+}
