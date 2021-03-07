8f20ddeec2e023271526345faaa7dc21da0e9aa7
Marcio Guilherme Bronzato de Avellar <mgb.avellar@gmail.com>
2021-03-07 15:05:33 -0300

    Tratando eventos JAVAFX - alert

--- Module-03/app20210307momento14h24m_EX_tratando_eventos_fx/src/application/ViewController.java
+++ Module-03/app20210307momento14h24m_EX_tratando_eventos_fx/src/application/ViewController.java
@@ -1,6 +1,8 @@
 package application;
 
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 
 public class ViewController {
@@ -10,7 +12,14 @@ public class ViewController {
 
     @FXML
     public void onBtTestAction() {
+
         // Geralmente, Actions correspondem a cliques do mouse que acionam uma ação
         System.out.println("click");
+
+        // Mostrando o Alert
+        Alerts.showAlert("Alert title", "Alert header", "Hello, guys!", Alert.AlertType.INFORMATION);
+
+        // ps. o header do alert pode ser null
+
     }
 }
